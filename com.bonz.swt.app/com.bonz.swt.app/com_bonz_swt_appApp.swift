1af056ac6a5e3d9fa7da4a70c8f1598473c02563
Rostyslav Dydiak <rostuslavdz@gmail.com>
2024-02-13 14:02:06 +0200

    initial project

---
+++ com.bonz.swt.app/com.bonz.swt.app/com_bonz_swt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_bonz_swt_appApp.swift
+//  com.bonz.swt.app
+//
+//  Created by Rostyslav on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct com_bonz_swt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
